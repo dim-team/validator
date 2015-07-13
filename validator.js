4bd4ab0cbda3535816776fb8f1a9935b22027a0f
JerroldLee <lijiayouyj@163.com>
2015-07-13 20:27:21 +0800

    update component of validator

--- validator.js
+++ validator.js
@@ -4,25 +4,50 @@
  * @email JerroldLi@163.com
  * @update 2015.07.13
  */
-var ValidatorTpl = __inline('validator.handlebars');
+var Poptips = require('poptips');
 
 var Validator = {
-	init: function (options) {
-		if (!this.status) {
-			this.options = options;
-			this.render();
-		}
-		
-	},
-	render: function () {
-		var ValidatorObj = $(ValidatorTpl()); 
-		this.ValidatorObj = ValidatorObj;
-		this.options.container.append(ValidatorObj);
-		this.status = 1;
+	init: function (container) {
+		this.container = container;
+
 		this.bindEvents();
 	},
 	bindEvents: function() {
-		
+		$('[data-validate="true"]', this.container).each(function() {
+			var $this = $(this);
+			var validateInfo = $(this).attr('data-validateInfo');
+			validateInfo = JSON.parse(validateInfo);
+			var options = {
+				container: $this,
+				autoHide: true
+			};
+			$this.on('focus', function() {
+				options.action = 'remove';
+				Poptips.init(options);
+			}).on('blur', function() {
+				var value = $this.val();
+				if (value && value.length > 0) {
+					$.each(validateInfo, function(i, n) {
+						var regx = new RegExp(n.regx);
+						var msg = n.msg;
+						console.log(regx.test(value));
+						if(!regx.test(value)) {
+							options.action = 'show';
+							options.status = 'error';
+							options.msg = msg;
+							Poptips.init(options);
+						}
+					});
+				} else if ($this.attr('data-necessary') == 'true') {
+					options.action = 'show';
+					options.status = 'error';
+					options.msg = '该字段不能为空';
+					Poptips.init(options);
+				}
+				
+			});
+
+		});
 	} 
 };
 

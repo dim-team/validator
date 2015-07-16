1a88ebd94a23599a9aa3607d36ed304a4fda3b0f
JerroldLee <lijiayouyj@163.com>
2015-07-16 15:15:32 +0800

    update version to 1.0.2

--- validator.js
+++ validator.js
@@ -7,47 +7,64 @@
 var Poptips = require('poptips');
 
 var Validator = {
-	init: function (container) {
-		this.container = container;
+	/*var validateOptions = {
+		container: this.doms.attributeContainer,//校验容器
+		auto: false//是否自动校验
+	};*/
+	init: function (validateOptions) {
+		this.validateOptions = validateOptions;
 
 		this.bindEvents();
 	},
 	bindEvents: function() {
-		$('[data-validate="true"]', this.container).each(function() {
+		var _this = this;
+		$('[data-validate="true"]', _this.validateOptions.container).each(function() {
 			var $this = $(this);
 			var validateInfo = $(this).attr('data-validateInfo');
 			validateInfo = JSON.parse(validateInfo);
 			var options = {
 				container: $this,
-				autoHide: true
+				autoHide: true,
+				validateInfo: validateInfo
 			};
-			$this.on('focus', function() {
-				options.action = 'remove';
-				Poptips.init(options);
-			}).on('blur', function() {
-				var value = $this.val();
-				if (value && value.length > 0) {
-					$.each(validateInfo, function(i, n) {
-						var regx = new RegExp(n.regx);
-						var msg = n.msg;
-						console.log(regx.test(value));
-						if(!regx.test(value)) {
-							options.action = 'show';
-							options.status = 'error';
-							options.msg = msg;
-							Poptips.init(options);
-						}
-					});
-				} else if ($this.attr('data-necessary') == 'true') {
+
+			if (_this.validateOptions.auto) {
+				_this.executeValidate(options);
+			} else {
+				$this.on('focus', function() {
+					options.action = 'remove';
+					Poptips.init(options);
+				}).on('blur', function() {
+					_this.executeValidate(options);	
+				});
+			}
+		});
+	},
+	executeValidate: function(options) {
+		var $this = options.container;
+		var validateInfo = options.validateInfo;
+		var value = $this.val();
+		if (value && value.length > 0) {
+			$.each(validateInfo, function(i, n) {
+				var regx = new RegExp(n.regx);
+				var msg = n.msg;
+				if(!regx.test(value)) {
+					$this.attr('data-status', 'false');
 					options.action = 'show';
 					options.status = 'error';
-					options.msg = '该字段不能为空';
+					options.msg = msg;
 					Poptips.init(options);
+				} else {
+					$this.attr('data-status', 'true');
 				}
-				
 			});
-
-		});
+		} else if ($this.attr('data-necessary') == 'true') {
+			$this.attr('data-status', 'false');
+			options.action = 'show';
+			options.status = 'error';
+			options.msg = '该字段不能为空';
+			Poptips.init(options);
+		}
 	} 
 };
 
